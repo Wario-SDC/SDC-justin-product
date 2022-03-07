3c70a8a8d8d362dcc776a562c58b7dd1886fca6b
Justin Greer <justin.t.greer1@gmail.com>
2022-03-07 12:01:05 -0800

    Test up

--- server/index.js
+++ server/index.js
@@ -1,28 +1,28 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const { JSDOM } = require("jsdom");
+const { window } = new JSDOM();
 const PORT = 3000 || process.env.PORT;
 const controller = require('./contrl');
+const directory = 'client/dist';
 
+//Middleware
 app.use(express.static('client/dist'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
+//Endpoints
 app.get('/products', controller.getAllProducts);
 app.get('/products/:id', controller.getProductReq);
 app.get('/products/:id/styles', controller.getStylesReq);
-// app.get('/products/:id/styles', controller.getStylesReq);
 
-
-// app.post(endpoint, (req, res) => {
-//   controller.addProduct(req, res);
-// });
-
-// explain (analyze, buffers, verbose, format text)
-// explain (analyze, buffers, verbose, format json)
-// EXPLAIN - runs estimated status
-// EXPLAIN ANALYZE - runs estimated and runs query
-//discard all
-
+//Set Up the Sever to Listen on Port 3000
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
 })
+
+//Export the App to the testing Library
+module.exports = {
+  app
+}
