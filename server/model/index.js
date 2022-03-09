9e03a9b72996253dbef2b4ddded8017caf21f884
Justin Greer <justin.t.greer1@gmail.com>
2022-03-09 15:30:24 -0800

    deploy

--- server/model/index.js
+++ server/model/index.js
@@ -117,3 +117,13 @@ module.exports = {
   }
 
 }//end of module exports
+
+
+// EXPLAIN ANALYZE
+// SELECT products.*, json_agg(
+//   json_build_object(
+//     'feature', features.feature,
+//     'value', features.value
+//   )
+// ) AS features FROM products JOIN features
+// ON products.id=features.product_id WHERE products.id=1 GROUP BY products.id
--- testing/k6.js
+++ testing/k6.js
@@ -1,36 +1,31 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
-import { Counter } from 'k6/metrics';
-
-let ErrorCount = new Counter("errors");
 
 export const options = {
-  vus: 100,
-  duration: '15s',
-  thresholds: {
-    errors: ["count<1"]
+  scenarios: {
+    constant_request_rate: {
+      executor: 'constant-arrival-rate',
+      rate: 1500,
+      timeUnit: '1s',
+      duration: '60s',
+      preAllocatedVUs: 100,
+      maxVUs: 100
+    }
   }
-};
-
-
-// export default function () {
-//   let res = http.get(`http://localhost:3000/products/1`);
-
-//   //Creating Error Check
-//   let success = check(res, {
-//     "status is 200": r => r.status === 200
-//   });
-//   if (!success) {
-//     // console.log('error');
-//     ErrorCount.add(1)
-//   }
-
-//   sleep(1);
-// }
-
+}
 
 export default function () {
-  const product = http.get(`http://localhost:3000/products/1`);
-  const styles = http.get(`http://localhost:3000/products/1/styles`);
-  sleep(1);
+  http.get(`http://localhost:3000/products/1`); //197 //83 //184
 }
+  // http.get(`http://localhost:3000/products/1/styles`); //140 //179
+
+  //Creating Error Check
+  // let success = check(res, {
+  //   "status is 200": r => r.status === 200
+  // });
+  // if (!success) {
+  //   // console.log('error');
+  //   ErrorCount.add(1)
+  // }
+
+  // sleep(1);
